18046489511d36cf6c9d6a05efe80c2d9aad5fa9
Kevin Tshibangu <kevintshib08@gmail.com>
2025-11-12 22:39:05 +0100

    testing new design

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </head>
-      <body className={isHomePage ? "home-page" : ""}>
+      <body className={isHomePage ? "home-page theme-rose" : "theme-rose"}>
       {/* <body> */}
         <Header />
         <main>{children}</main>
